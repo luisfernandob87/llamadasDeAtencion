4a78ce8290391260969972820f59ace542663769
luisfernandob87 <luisfernandob87@gmail.com>
2023-05-16 09:25:58 -0600

    add details

--- src/components/DetalleLlamada.jsx
+++ src/components/DetalleLlamada.jsx
@@ -1,10 +1,8 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
-import SignatureCanvas from "react-signature-canvas";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
 import "../crearLlamada.css";
+import logo from "../assets/logo.png";
 import moment from "moment";
-import MenuTop from "./MenuTop";
 moment.locale("es");
 
 function DetalleLlamada() {
@@ -25,56 +23,57 @@ function DetalleLlamada() {
         `http://localhost:1337/api/llamadade-atencions/${idDetalle}?populate=*`,
         config
       )
-      .then((res) => setInfo(res.data.data.attributes));
+      .then((res) => setInfo(res.data.data));
   }, []);
 
   console.log(info);
   return (
     <>
-      <MenuTop />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <form style={{ maxWidth: 800 }}>
-          <h2>FORMATO DE ASESORÍA PARA MEJORAR</h2>
-          {/* <p>Fecha: {info.attributes.createdAt}</p> */}
+          <h2 style={{ marginTop: 0 }}>FORMATO DE ASESORÍA PARA MEJORAR</h2>
+          <p>
+            Fecha:{" "}
+            <strong>
+              {moment(info.attributes?.createdAt).format("DD/MM/YYYY HH:MM")}
+            </strong>
+          </p>
           <div className="container">
-            <p htmlFor="selectEmpleado">Nombre colaborador (a): </p>
-            {/* <p>{info.empleado.data.attributes.nombreCompleto}</p> */}
+            <p>
+              Nombre colaborador (a):{" "}
+              <strong>
+                {info.attributes?.empleado.data.attributes.nombreCompleto}
+              </strong>
+            </p>
           </div>
           <div className="container">
-            <label htmlFor="departamento">Departamento: </label>
-            <select id="departamento"></select>
+            <p>
+              Departamento:{" "}
+              <strong>
+                {info.attributes?.departamento.data.attributes.descripcion}
+              </strong>
+            </p>
           </div>
           <div className="container">
-            <label htmlFor="puesto">Puesto: </label>
-            <select id="puesto"></select>
+            <p>
+              Puesto:{" "}
+              <strong>
+                {info.attributes?.puesto.data.attributes.descripcion}
+              </strong>
+            </p>
           </div>
           <div className="container">
-            <label htmlFor="grado">Tipo de Llamada de Atención: </label>
-            <select id="grado">
-              <option></option>
-              <option value="Llamada de atención verbal">
-                Llamada de atención verbal
-              </option>
-              <option value="Llamada de atención escrita">
-                Llamada de atención escrita
-              </option>
-              <option value="Suspensión">Suspensión</option>
-            </select>
+            <p>
+              Tipo de Llamada de Atención:{" "}
+              <strong>{info.attributes?.grado}</strong>
+            </p>
           </div>
           <div className="container">
-            <p htmlFor="descripcion">
+            <p>
               De acuerdo a las obligaciones de su puesto hacemos la presenta
               llamada de atención por el motivo siguiente:
             </p>
-            <textarea
-              style={{
-                width: "100%",
-                height: 50,
-              }}
-              id="descripcion"
-              type="text"
-              placeholder="Descripción"
-            />
+            <strong>{info.attributes?.descripcion}</strong>
             <p>
               Debido a que la falta afecta su desempeño, debe mejorar en este
               aspecto, con el fin de no afectar su normal desempeño y el
@@ -85,68 +84,57 @@ function DetalleLlamada() {
               colaborador(a) asesorado.
             </p>
           </div>
-          <h3>PLAN DE ACCIÓN</h3>
-          <p>
+          <h3 style={{ margin: 0 }}>PLAN DE ACCIÓN</h3>
+          <p style={{ margin: 0 }}>
             Acciones que se seguirán para corregir este comportamiento o
             desempeño, segundo y tercer nivel de asesoria (usar hoja anexa en
             caso necesario)
           </p>
           <div style={{ display: "flex" }}>
             <div style={{ display: "grid", width: "33%" }}>
-              <label htmlFor="fechaImplementacion">
-                Fecha de Implementación
-              </label>
-              <input
-                style={{ height: 50 }}
-                id="fechaImplementacion"
-                type="date"
-              />
+              <p>Fecha de Implementación</p>
+              <strong>
+                {moment(info.attributes?.fechaImplementacion).format(
+                  "DD/MM/YYYY"
+                )}
+              </strong>
             </div>
             <div style={{ display: "grid", width: "33%" }}>
-              <label htmlFor="accionCorrectiva">Acción Correctiva</label>
-              <textarea
-                id="accionCorrectiva"
-                style={{ height: 50 }}
-                type="text"
-                placeholder="Acción Correctiva"
-              />
+              <p>Acción Correctiva</p>
+              <strong>{info.attributes?.accionCorrectiva}</strong>
             </div>
             <div style={{ display: "grid", width: "33%" }}>
-              <label htmlFor="compromiso">Compromiso</label>
-              <textarea
-                style={{ height: 50 }}
-                id="compromiso"
-                type="text"
-                placeholder="Compromiso"
-              />
+              <p>Compromiso</p>
+              <strong>{info.attributes?.compromiso}</strong>
             </div>
           </div>
           <div className="container">
             <p>
               En caso de no mejorar este comportamiento o desempeño, el
               siguiente nivel de llamada de atención será:{" "}
+              <strong>{info.attributes?.proximoGrado}</strong>
             </p>
-            <input
-              style={{ width: 200, marginBottom: 20 }}
-              placeholder="Próximo llamado de atención"
-              type="text"
-            ></input>
           </div>
           <div style={{ display: "flex", gap: 10 }}>
             <div
               style={{
                 display: "grid",
                 width: "20%",
+                textAlign: "center",
+                height: 175,
               }}
             >
-              <p style={{ textAlign: "center" }}>
+              <p style={{ textAlign: "center", margin: 0 }}>
                 Duración del compromiso de llamada de atención
               </p>
-              <label htmlFor="inicio">Inicio</label>
-              <input id="inicio" type="date" />
-
-              <label htmlFor="final">Final</label>
-              <input id="final" type="date" />
+              <p style={{ textAlign: "center", margin: 0 }}>Inicio</p>
+              <strong>
+                {moment(info.attributes?.inicioCompromiso).format("DD/MM/YYYY")}
+              </strong>
+              <p style={{ textAlign: "center", margin: 0 }}>Final</p>
+              <strong>
+                {moment(info.attributes?.finalCompromiso).format("DD/MM/YYYY")}
+              </strong>
             </div>
             <div
               style={{
@@ -155,17 +143,14 @@ function DetalleLlamada() {
                 borderStyle: "solid",
                 borderRadius: 10,
                 alignSelf: "center",
+                height: 175,
               }}
             >
-              <label htmlFor="">Firma Colaborador(a)</label>
-              <SignatureCanvas
-                penColor="black"
-                backgroundColor="#f6f6f9"
-                canvasProps={{
-                  height: 100,
-                  className: "sigCanvas",
-                  width: "200%",
-                }}
+              <p style={{ textAlign: "center" }}>Firma Colaborador(a)</p>
+              <img
+                style={{ maxWidth: "90%" }}
+                src={info.attributes?.firmaColaborador}
+                alt="Firma Colaborador"
               />
             </div>
             <div
@@ -174,17 +159,15 @@ function DetalleLlamada() {
                 borderStyle: "solid",
                 borderRadius: 10,
                 borderWidth: 1,
+                alignSelf: "center",
+                height: 175,
               }}
             >
-              <label htmlFor="">Firma Jefe Inmediato</label>
-              <SignatureCanvas
-                penColor="black"
-                backgroundColor="#f6f6f9"
-                canvasProps={{
-                  height: 100,
-                  className: "sigCanvas",
-                  width: "200%",
-                }}
+              <p style={{ textAlign: "center" }}>Firma Jefe Inmediato</p>
+              <img
+                style={{ maxWidth: "90%" }}
+                src={info.attributes?.firmaJefeInmediato}
+                alt="Firma Jefe Inmediato"
               />
             </div>
             <div
@@ -193,17 +176,19 @@ function DetalleLlamada() {
                 borderStyle: "solid",
                 borderRadius: 10,
                 borderWidth: 1,
+                alignSelf: "center",
+                height: 175,
               }}
             >
-              <label htmlFor="">Firma Recursos Humanos</label>
-              <SignatureCanvas
-                penColor="black"
-                backgroundColor="#f6f6f9"
-                canvasProps={{
-                  height: 100,
-                  className: "sigCanvas",
-                  width: "200%",
-                }}
+              <p style={{ textAlign: "center" }}>Firma Recursos Humanos</p>
+              <img
+                style={{ maxWidth: "90%" }}
+                src={
+                  info.attributes?.firmaRrhh === ""
+                    ? logo
+                    : info.attributes?.firmaRrhh
+                }
+                alt="Firma Recursos Humanos"
               />
             </div>
           </div>
